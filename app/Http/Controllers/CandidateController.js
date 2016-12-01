d9f01fae36425b788063599a0160d13306969d00
nechor <dariusz.jakubowski@gmail.com>
2016-12-01 06:55:38 +0100

    Cos tam popisalem

--- app/Http/Controllers/CandidateController.js
+++ app/Http/Controllers/CandidateController.js
@@ -11,20 +11,19 @@ class CandidateController {
     }
 
   * show (request, response) {
-      const candidate = yield Database
-        .from('candidates')
+      const candidate = yield Candidate.query()
         .where('id', request.param('id'))
         .first()
-      console.log("kandydat", candidate)
+      //console.log("kandydat", candidate)
       yield response.sendView('cand/show', { candidate })
     }
 
   * edit (request, response) {
-    const candidate = yield Database
-      .from('candidates')
+    const candidate = yield Candidate.query()
       .where('id', request.param('id'))
       .first()
-    console.log("kandydat", candidate)
+    //console.log("kandydat", candidate)
+    //console.log("QQQQQQQQQQQQ", candidate)
     yield response.sendView('cand/edit', { candidate })
     }
 
@@ -37,15 +36,23 @@ class CandidateController {
 
   //zapisanie nowego kolesia
   * store (request, response) {
-      const candidate = yield Database
-        .from('candidates')
-        .where('id', 1)
-      yield response.sendView('cand/create')
+      const candidateData = request.only('name', 'lastname')
+      yield Candidate.create(candidateData)
+      response.redirect('/cand/list')
     }
 
   //zapisanie po edycji
   * update (request, response) {
-      return response.redirect('/')
+    const candidate = yield Candidate.query()
+      .where('id', request.param('id'))
+      .first()
+    console.log("-------------------------", request.param('id'));
+    const candidateData = request.only('name', 'lastname')
+    candidate.name = candidateData.name
+    candidate.lastname = candidateData.lastname
+
+    yield candidate.save()
+    response.redirect('/cand/list')
     }
 }
 
